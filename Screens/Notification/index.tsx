2a50d6087a829ee28d061d544ecf722c8f13678a
Shaikh Mubashir <39335986+shaikh99mubashir@users.noreply.github.com>
2023-06-17 14:02:11 +0500

    update

--- Screens/Notification/index.tsx
+++ Screens/Notification/index.tsx
@@ -6,7 +6,9 @@ import {
   Text,
   View,
   ScrollView,
-  ToastAndroid
+  ToastAndroid,
+  TouchableOpacity,
+  Modal,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import Header from '../../Components/Header';
@@ -15,11 +17,12 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {useDispatch, useSelector} from 'react-redux';
 import axios from 'axios';
-import { BaseUrl } from '../../Constants/BaseUrl';
-import { logout } from '../../Redux/Reducer/Reducers';
+import {BaseUrl} from '../../Constants/BaseUrl';
+import {logout} from '../../Redux/Reducer/Reducers';
 const Notification = ({navigation}: any) => {
   const [userNotificarion, setUserNotificarion] = useState([]);
   const noti: any = useSelector(notification => notification);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
   useEffect(() => {
     setUserNotificarion(noti.user.notification);
   }, []);
@@ -28,50 +31,36 @@ const Notification = ({navigation}: any) => {
 
   const updateNotificationStatus = () => {
     const formData = new FormData();
-    userNotificarion && userNotificarion.map((e:any) => {
+    userNotificarion &&
+      userNotificarion.map((e: any) => {
+        formData.append('notification_id', e.id);
 
-      formData.append('notification_id', e.id);
-    
-  
-    const config = {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    };
-  
-    axios
-      .post(`${BaseUrl}updateNotificationStatus`, formData, config)
-      .then(({ data }) => {
-        console.log('data', data);
-      })
-      .catch(error => {
-        ToastAndroid.show(
-          'Internal Server Error in Notification id',
-          ToastAndroid.BOTTOM,
-        );
+        const config = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        };
+
+        axios
+          .post(`${BaseUrl}updateNotificationStatus`, formData, config)
+          .then(({data}) => {
+            console.log('data', data);
+          })
+          .catch(error => {
+            ToastAndroid.show(
+              'Internal Server Error in Notification id',
+              ToastAndroid.BOTTOM,
+            );
+          });
       });
-
-    });
   };
 
-  useEffect(()=>{
-    updateNotificationStatus()
-  },[])
- 
+  // useEffect(()=>{
+  //   updateNotificationStatus()
+  // },[])
 
-  const renderNotificationItems = ({item}: any): any => {
-    return (
-      <View
-        style={{
-          backgroundColor: 'white',
-          shadowOpacity: 1,
-          shadowRadius: 10,
-          borderRadius: 10,
-          elevation: 2,
-          marginBottom: 10,
-        }}>
-        <View style={{flexDirection: 'row', padding: 8, gap: 10}}>
-          <View
+  {
+    /* <View
             style={{
               backgroundColor: Color.mainColor,
               padding: 10,
@@ -79,39 +68,110 @@ const Notification = ({navigation}: any) => {
               height: 50,
             }}>
             <FontAwesome name="bell" size={30} color="white" />
+          </View> */
+  }
+  const [modalData, setModalData] = useState('')
+  const NotificationModal = (item: any) => {
+    setModalVisible(true)
+    const modalData = JSON.parse(item)
+    setModalData(modalData)
+
+  };
+
+  console.log('modalData',modalData);
+  
+  const renderNotificationItems = ({item}: any): any => {
+    return (
+      <>
+        <Text
+          style={{
+            color: Color.mainColor,
+            fontSize: 14,
+            fontWeight: '700',
+            marginBottom: 3,
+            textAlign:'left'
+          }}>
+          {/* {item.created_at} */}
+          17th Jun 2023
+        </Text>
+      <View style={{alignItems:'center'}}>
+        <TouchableOpacity
+          onPress={() => NotificationModal(item)}
+          activeOpacity={0.8}
+          style={{
+            backgroundColor: 'white',
+            // shadowOpacity: 1,
+            shadowRadius: 10,
+            borderRadius: 5,
+            elevation: 2,
+            marginBottom: 10,
+            width: '99%',
+            paddingHorizontal:8,
+            paddingVertical:12
+          }}>
+          <View style={{flexDirection: 'row', gap: 10}}>
+            <View style={{flexDirection: 'column', width: '85%'}}>
+              <Text
+                style={{
+                  color: Color.textColor,
+                  fontSize: 14,
+                  fontWeight: '700',
+                }}>
+                title
+                {/* {item.name} */}
+              </Text>
+              <Text
+                style={{
+                  color: 'grey',
+                  fontSize: 14,
+                  paddingRight: 10,
+                }}>
+                {item.message.length > 35
+                  ? `${item.message.slice(0, 35).trim()}...`
+                  : item.message.trim()}
+              </Text>
+            </View>
+            <View
+              style={{justifyContent: 'space-between', alignItems: 'center'}}>
+              <Text
+                style={{
+                  paddingHorizontal: 8,
+                  borderRadius: 8,
+                  color: 'grey',
+                  fontSize: 12,
+                }}>
+                11:38
+              </Text>
+              {item.status == 'New' ? (
+                <Text
+                  style={{
+                    backgroundColor: Color.mainColor,
+                    paddingHorizontal: 8,
+                    borderRadius: 8,
+                    color: 'white',
+                    fontSize: 12,
+                  }}>
+                  {item.status}
+                </Text>
+              ) : (
+                ''
+              )}
+            </View>
           </View>
-          <View style={{flexDirection: 'column',width:width/1.4}}>
-            <Text
-              style={{
-                color: Color.mainColor,
-                fontSize: 16,
-                fontWeight: 'bold',
-              }}>
-              title
-              {/* {item.name} */}
-            </Text>
-          <Text
-            style={{
-              color: Color.textColor,
-              fontSize: 16,
-              textAlign: 'justify',
-              paddingRight: 10,
-            }}>
-            {item.message}
-          </Text>
-              </View>
-        </View>
+          {/* <Text style={{color: 'grey', fontSize: 12}}>{item.created_at}</Text> */}
+        </TouchableOpacity>
       </View>
+      </>
     );
   };
   return (
     <View
       style={{
         backgroundColor: Color.white,
-        flex:1,
+        flex: 1,
         paddingHorizontal: 15,
-        alignItems: 'center',
-        paddingBottom:20
+        // alignItems: 'center',
+        // paddingBottom: 20,
       }}>
       <Header navigation={navigation} backBtn noLogo />
       <Text
@@ -147,7 +207,31 @@ const Notification = ({navigation}: any) => {
           </Text>
         </View>
       )}
-      
+
+      {/* <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 10,
+              width: '90%',
+              // paddingHorizontal:15,
+              paddingVertical:15
+            }}>
+              <View style={{borderBottomWidth:1, borderBottomColor:'grey'}}>
+              <Text style={{textAlign:'center',fontSize:20, marginBottom:10, color:Color.mainColor, fontWeight:'700' }}>Notification Details</Text>
+              </View>
+              <Image source={{uri: modalData.image}} style={{width:150,height:150, borderWidth:1}} resizeMode='contain'/>
+              <Text>{modalData.message}</Text>
+            </View>
+        </View>
+      </Modal> */}
     </View>
   );
 };
